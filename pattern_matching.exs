b70dfef34d2bf817d6365a35c37117914899f67d
nattatorn.dev <nattatorn.dev@gmail.com>
2017-08-04 22:32:50 +0700

    🎉 Initialize Commit

---
+++ pattern_matching.exs
@@ -0,0 +1,22 @@
+{color, place} = {"Green", "Belgium"}
+
+IO.puts color
+IO.puts place
+
+[a, b, c] = [1, 89, 45]
+
+IO.puts a
+IO.puts b
+IO.puts c
+
+# Capitales
+
+{colombia, francia, austria, belgica} = {"Bogotá", "París", "Vienna", "Bruselas"}
+
+IO.puts colombia #=> Bogotá
+IO.puts francia #=> París
+IO.puts austria #=> Vienna
+IO.puts belgica #=> Bruselas
+
+"Mauricio " <> last_name = "Mauricio Serna"
+IO.puts last_name
